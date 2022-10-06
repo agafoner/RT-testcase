a7e97f3c1f81e739364ff8ac07f33494fb86ffa4
GrayStr <agafoner@gmail.com>
2022-10-06 18:26:29 +0500

    1 Добавил функционал возвращение в вышестоящую папку

--- client/src/components/panel/ElList.vue
+++ client/src/components/panel/ElList.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="el-List" >
-
-  <FileElement v-for="row in $props.dirList" v-if="!!$props.dirList" :row="row" @chDir="chDir"></FileElement>
+  <div v-if="!!upperDir" class="inline" @dblclick="changeDir(upperDir+'/')">
+    {{ upperDir }}
+  </div>
+  <FileElement v-for="row in $props.dirList" v-if="!!$props.dirList" :row="row" @chDir="selectDir"></FileElement>
 </div>
 </template>
 <script lang="ts">
@@ -23,13 +25,27 @@ export default defineComponent({
   },
 
   methods: {
-    chDir: function (name: string, type: string) {
-      console.log('chDir type: ', type)
+    selectDir: function (name: string, type: string) {
       if (type!='folder') return
       const path=this.$store.state.currentDir[this.$props.panelId]+name
-      console.log('chDir path:  ', path)
-      this.$store.state.fetchDirList(path,this.$props.panelId)
+      this.changeDir(path)
+    },
+    changeDir: function (dir: string) {
+      this.$store.state.fetchDirList(dir,this.$props.panelId)
     }
+  },
+  computed: {
+      upperDir () : string | boolean {
+        console.log('upperDir')
+        const str=this.$store.state.currentDir[this.$props.panelId]
+        if (!str) return false
+        let arr=str.replaceAll('\\','/').split('/')
+        arr.pop()
+        console.log(arr)
+        if (str.length<=1) return false
+        arr.pop()
+        return arr.join('/');
+      }
   }
 
 })
